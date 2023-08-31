e8f88fb851d32dfd6f1c8b1ac6a71b9a41547273
AnjaliP <anju.phaltane@gmail.com>
2023-08-30 21:16:47 -0700

    Arraylist of Integer Created

---
+++ GitDemo1/src/com/velocity/self/AddArrayList.java
@@ -0,0 +1,20 @@
+package com.velocity.self;
+
+import java.util.ArrayList;
+import java.util.TreeSet;
+
+public class AddArrayList {
+
+	public static void main(String[] args) {
+		ArrayList <Integer> arrayList=new ArrayList <Integer>();
+		arrayList.add(10);
+		arrayList.add(200);
+		arrayList.add(39);
+		for(int i:arrayList) {
+			System.out.println(i);
+			
+			
+	
+	}
+}
+}
