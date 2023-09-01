e74686138b30340ba44b8044183f4218d8aca514
AnjaliP <anju.phaltane@gmail.com>
2023-08-31 22:55:04 -0700

    Addition of two numbers

---
+++ CalculatorOperations/src/com/anjali/self/AddOperation.java
@@ -0,0 +1,12 @@
+//Addition of two numbers
+package com.anjali.self;
+
+public class AddOperation {
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int result=a+b;
+		System.out.println("The Addition is:"+result);
+	}
+
+}
